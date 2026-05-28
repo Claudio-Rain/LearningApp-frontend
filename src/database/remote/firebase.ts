ee6d75804ef01c8599b78b0458a814c5822e6a77
rainclaudio <rainclaudio25@gmai.com>
2026-05-27 22:11:29 -0400

    Deployment

--- src/database/remote/firebase.ts
+++ src/database/remote/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCMwyu5jelRDJ39rEeq0_huAntu52ne8EQ",
-  authDomain: "learningapp-f7d22.firebaseapp.com",
-  projectId: "learningapp-f7d22",
-  storageBucket: "learningapp-f7d22.firebasestorage.app",
-  messagingSenderId: "89331298111",
-  appId: "1:89331298111:web:91475d009ad1af427a2d8a"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
